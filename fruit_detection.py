2ebe8504288c30b7acfe689b7bb06d506eecf721
AdnanShariar <48999376+AdnanShariar@users.noreply.github.com>
2019-12-23 00:55:49 +0600

    Add files via upload

--- fruit_detection.py
+++ fruit_detection.py
@@ -7,9 +7,14 @@ This is a temporary script file.
 import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
+from glob import glob
 from cvlib.object_detection import draw_bbox
-im = cv2.imread('11.jpg')
-bbox, label, conf = cv.detect_common_objects(im)
-output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
-plt.show()
+
+img_names = glob('dataset/*.jpg')
+images = [cv2.imread(img) for img in img_names]
+
+for img in images:
+    bbox, label, conf = cv.detect_common_objects(img)
+    output_image = draw_bbox(img, bbox, label, conf)
+    plt.imshow(output_image)
+    plt.show()
